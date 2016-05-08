fe49200273815897a2e6e26b91ff20e947c75d04
yoursxin <yours.xin@gmail.com>
2016-05-08 23:04:28 +0800

    add customers search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,22 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all.limit(10)
+    if params[:keywords].present?
+      @customers = Customer.where(" first_name like :first_name "+
+                                  " or last_name like :last_name "+
+                                  " or email like :email ",{
+                                    first_name: params[:keywords]+"%",
+                                    last_name: params[:keywords]+"%",
+                                    email: params[:keywords]+"%"
+                                  }
+
+        )
+      puts "--hxx customers: #{@customers} "
+    else
+      @customers = []
+    end
+
+
+
+    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-30.times do |i|
+300.times do |i|
   Customer.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
