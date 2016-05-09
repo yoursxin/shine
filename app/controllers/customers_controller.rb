c7c62d0bf5ba1cdac19a9080700b0edf0dca2a3d
yoursxin <yours.xin@gmail.com>
2016-05-09 22:31:10 +0800

    modify the style of customers result,add pager

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10 
   def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
       @customers = Customer.where(" first_name like :first_name "+
                                   " or last_name like :last_name "+
@@ -7,9 +9,12 @@ class CustomersController < ApplicationController
                                     first_name: params[:keywords]+"%",
                                     last_name: params[:keywords]+"%",
                                     email: params[:keywords]+"%"
-                                  }
+                                  }).order(" email ")
+                                  .offset(PAGE_SIZE * @page)
+                                  .limit(PAGE_SIZE)
 
-        )
+
+        
       puts "--hxx customers: #{@customers} "
     else
       @customers = []
